98466f349c7dd79bf2d21e0d46f68ba14aaa07c6
danielrasmuson <danielrasmuson@gmail.com>
2014-08-02 00:12:16 -0400

    wow! I got the gets and posts working. I'm so happy :D

--- web/testFlask.py
+++ web/testFlask.py
@@ -1,4 +1,6 @@
-from flask import Flask
+# for the full tutorial visit this site
+# http://flask.pocoo.org/docs/quickstart
+from flask import Flask, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,9 +19,27 @@ def show_user_profile(username):
 # /post/12 -- good
 # /post/hello -- 404 error
 @app.route('/post/<int:post_id>')
+# can use
+# int   accepts integers
+# float   like int but for floating point values
+# path    like the default but also accepts slashes
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
+
+@app.route('/urlPrint')
+def printUrl():
+    # using the function names
+    return url_for("hello")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # do_the_login()
+        return "you sent a post"
+    else:
+        # show_the_login_form()
+        return "you sent a get"
 
 
 if __name__ == '__main__':
