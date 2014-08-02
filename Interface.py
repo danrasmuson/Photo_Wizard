894c75cd5ba08a392caacc14a664ef02abc59b51
danielrasmuson <danielrasmuson@gmail.com>
2014-08-01 21:33:59 -0400

    not much progress going to take a break

--- Interface.py
+++ Interface.py
@@ -1,5 +1,5 @@
 import wxversion
-import wx, wx.html
+import wx, wx.html, wx.grid
 import sys
 
 class HtmlWindow(wx.html.HtmlWindow):
@@ -30,36 +30,49 @@ class AboutBox(wx.Dialog):
 
 class Frame(wx.Frame):
     def __init__(self, title):
-        wx.Frame.__init__(self, None, title=title, pos=(150,150), size=(350,200))
+        wx.Frame.__init__(self, None, title=title, pos=(150,150), size=(600,600))
         self.Bind(wx.EVT_CLOSE, self.OnClose)
+        self.panel = wx.Panel(wx.Frame)
 
-        menuBar = wx.MenuBar()
-        menu = wx.Menu()
-        m_exit = menu.Append(wx.ID_EXIT, "E&xit\tAlt-X", "Close window and exit program.")
-        self.Bind(wx.EVT_MENU, self.OnClose, m_exit)
-        menuBar.Append(menu, "&File")
-        menu = wx.Menu()
-        m_about = menu.Append(wx.ID_ABOUT, "&About", "Information about this program")
-        self.Bind(wx.EVT_MENU, self.OnAbout, m_about)
-        menuBar.Append(menu, "&Help")
-        self.SetMenuBar(menuBar)
+        # menuBar = wx.MenuBar()
+        # menu = wx.Menu()
+        # m_exit = menu.Append(wx.ID_EXIT, "E&xit\tAlt-X", "Close window and exit program.")
+        # self.Bind(wx.EVT_MENU, self.OnClose, m_exit)
+        # menuBar.Append(menu, "&File")
+        # menu = wx.Menu()
+        # m_about = menu.Append(wx.ID_ABOUT, "&About", "Information about this program")
+        # self.Bind(wx.EVT_MENU, self.OnAbout, m_about)
+        # menuBar.Append(menu, "&Help")
+        # self.SetMenuBar(menuBar)
 
-        self.statusbar = self.CreateStatusBar()
+        # self.statusbar = self.CreateStatusBar()
+        self.frame = wx.Frame(None, title='Photo Control')
+        # self.panel = wx.Panel(self.frame)
+ 
+        # self.PhotoMaxSize = 240
+ 
+        self.createWidgets()
+        self.frame.Show()
 
-        panel = wx.Panel(self)
+
+
+
         box = wx.BoxSizer(wx.VERTICAL)
-
-        self.m_text = wx.StaticText(panel, -1, "Hello World!")
+        self.m_text = wx.TextCtrl(self.panel, -1, "Hello World!")
         self.m_text.SetFont(wx.Font(14, wx.SWISS, wx.NORMAL, wx.BOLD))
         self.m_text.SetSize(self.m_text.GetBestSize())
         box.Add(self.m_text, 0, wx.ALL, 10)
 
-        m_close = wx.Button(panel, wx.ID_CLOSE, "Close")
-        m_close.Bind(wx.EVT_BUTTON, self.OnClose)
-        box.Add(m_close, 0, wx.ALL, 10)
+        # grid = SimpleGrid(self)
+        self.createWidgets()
 
-        panel.SetSizer(box)
-        panel.Layout()
+
+        # m_close = wx.Button(panel, wx.ID_CLOSE, "Close")
+        # m_close.Bind(wx.EVT_BUTTON, self.OnClose)
+        # box.Add(m_close, 0, wx.ALL, 10)
+
+        self.panel.SetSizer(box)
+        self.panel.Layout()
 
     def setBodyText(self, message):
         self.m_text.SetLabel(message)
@@ -78,8 +91,54 @@ class Frame(wx.Frame):
         dlg.ShowModal()
         dlg.Destroy()
 
+    def createWidgets(self):
+        img = wx.EmptyImage(240,240)
+        self.imageCtrl = wx.StaticBitmap(self.panel, wx.ID_ANY, 
+                                         wx.BitmapFromImage(img))
+    
+        self.mainSizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
+    
+        self.mainSizer.Add(wx.StaticLine(self.panel, wx.ID_ANY),
+                           0, wx.ALL|wx.EXPAND, 5)
+        self.mainSizer.Add(self.imageCtrl, 0, wx.ALL, 5)
+        self.mainSizer.Add(self.sizer, 0, wx.ALL, 5)
+    
+        self.panel.SetSizer(self.mainSizer)
+        self.mainSizer.Fit(self.frame)
+    
+        self.panel.Layout()
+
+    
+    def setPhoto(self, filepath):
+        img = wx.Image(filepath, wx.BITMAP_TYPE_ANY)
+        # scale the image, preserving the aspect ratio
+        W = img.GetWidth()
+        H = img.GetHeight()
+        if W > H:
+            NewW = self.PhotoMaxSize
+            NewH = self.PhotoMaxSize * H / W
+        else:
+            NewH = self.PhotoMaxSize
+            NewW = self.PhotoMaxSize * W / H
+        img = img.Scale(NewW,NewH)
+    
+        self.imageCtrl.SetBitmap(wx.BitmapFromImage(img))
+        self.panel.Refresh()
+
 app = wx.App(redirect=True)   # Error messages go to popup window
 top = Frame("<<project>>")
 top.setBodyText("daniel")
 top.Show()
 app.MainLoop()
+
+# class TestFrame(wx.Frame):
+#     def __init__(self, parent):
+#         wx.Frame.__init__(self, parent, -1, "A Grid",
+#                 size=(275, 275))
+        # grid = SimpleGrid(self)
+
+# app = wx.PySimpleApp()
+# frame = TestFrame(None)
+# frame.Show(True)
+# app.MainLoop()
--- photoViewer.py
+++ photoViewer.py
@@ -4,6 +4,8 @@ import wx
 class PhotoCtrl(wx.App):
     def __init__(self, redirect=False, filename=None):
         wx.App.__init__(self, redirect, filename)
+        # wx.Frame.__init__(self, None, title=title, pos=(150,150), size=(600,600))
+
         self.frame = wx.Frame(None, title='Photo Control')
  
         self.panel = wx.Panel(self.frame)
@@ -12,27 +14,25 @@ class PhotoCtrl(wx.App):
  
         self.createWidgets()
         self.frame.Show()
+
+        # input box
+        box = wx.BoxSizer(wx.VERTICAL)
+        self.m_text = wx.TextCtrl(self.panel, -1, "Hello World!")
+        self.m_text.SetFont(wx.Font(10, wx.SWISS, wx.NORMAL, wx.BOLD))
+        self.m_text.SetSize((200,100))
+        box.Add(self.m_text, 0, wx.ALL, 10)
  
     def createWidgets(self):
-        instructions = 'Browse for an image'
         img = wx.EmptyImage(240,240)
         self.imageCtrl = wx.StaticBitmap(self.panel, wx.ID_ANY, 
                                          wx.BitmapFromImage(img))
- 
-        # instructLbl = wx.StaticText(self.panel, label=instructions)
-        # self.photoTxt = wx.TextCtrl(self.panel, size=(200,-1))
-        # browseBtn = wx.Button(self.panel, label='Browse')
-        # browseBtn.Bind(wx.EVT_BUTTON, self.onBrowse)
  
         self.mainSizer = wx.BoxSizer(wx.VERTICAL)
         self.sizer = wx.BoxSizer(wx.HORIZONTAL)
  
         self.mainSizer.Add(wx.StaticLine(self.panel, wx.ID_ANY),
                            0, wx.ALL|wx.EXPAND, 5)
-        # self.mainSizer.Add(instructLbl, 0, wx.ALL, 5)
         self.mainSizer.Add(self.imageCtrl, 0, wx.ALL, 5)
-        # self.sizer.Add(self.photoTxt, 0, wx.ALL, 5)
-        # self.sizer.Add(browseBtn, 0, wx.ALL, 5)        
         self.mainSizer.Add(self.sizer, 0, wx.ALL, 5)
  
         self.panel.SetSizer(self.mainSizer)
@@ -40,24 +40,8 @@ class PhotoCtrl(wx.App):
  
         self.panel.Layout()
 
-        self.onView("c:\Users\Daniel\Pictures\desk1.JPG")
  
-    def onBrowse(self, event):
-        """ 
-        Browse for file
-        """
-        wildcard = "JPEG files (*.jpg)|*.jpg"
-        dialog = wx.FileDialog(None, "Choose a file",
-                               wildcard=wildcard,
-                               style=wx.OPEN)
-        if dialog.ShowModal() == wx.ID_OK:
-            self.photoTxt.SetValue(dialog.GetPath())
-        dialog.Destroy() 
-        self.onView()
- 
-    def onView(self, filepath):
-        # filepath = self.photoTxt.GetValue()
-
+    def setPhoto(self, filepath):
         img = wx.Image(filepath, wx.BITMAP_TYPE_ANY)
         # scale the image, preserving the aspect ratio
         W = img.GetWidth()
@@ -75,4 +59,5 @@ class PhotoCtrl(wx.App):
  
 if __name__ == '__main__':
     app = PhotoCtrl()
+    app.setPhoto("c:\Users\Daniel\Pictures\desk1.JPG")
     app.MainLoop()
