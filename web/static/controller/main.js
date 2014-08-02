f39a346a646c47384fd6870a3277af66a1ea3b53
danielrasmuson <danielrasmuson@gmail.com>
2014-08-02 15:31:09 -0400

    moved things back into index.html

--- web/static/controller/main.js
+++
@@ -1,7 +0,0 @@
-angular.module('app', []);
-
-angular.module('app')
-    .controller('PageController', function($scope) {
-        $scope.imageTemplate = "views/images.html";
-        $scope.errorTemplate = "views/error.html";
-    });
